a8182c305b3291bbb4e09dae25daced36e7127f6
snuber-w <106393188+snuber-w@users.noreply.github.com>
2022-08-11 15:57:41 +0300

    First commit

---
+++ app/controllers/CartController.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\models\Cart;
+use wfm\App;
+
+/** @property Cart $model */
+class CartController extends AppController
+{
+
+    public function addAction(): bool
+    {
+        $lang = App::$app->getProperty('language');
+        $id = get('id');
+        $qty = get('qty');
+
+        if (!$id) {
+            return false;
+        }
+
+        $product = $this->model->get_product($id, $lang);
+        if (!$product) {
+            return false;
+        }
+
+        $this->model->add_to_cart($product, $qty);
+        if ($this->isAjax()) {
+            $this->loadView('cart_modal');
+        }
+        redirect();
+        return true;
+    }
+
+    public function showAction()
+    {
+        $this->loadView('cart_modal');
+    }
+
+    public function deleteAction()
+    {
+        $id = get('id');
+        if (isset($_SESSION['cart'][$id])) {
+            $this->model->delete_item($id);
+        }
+        if ($this->isAjax()) {
+            $this->loadView('cart_modal');
+        }
+        redirect();
+    }
+
+    public function clearAction()
+    {
+        if (empty($_SESSION['cart'])) {
+            return false;
+        }
+        unset($_SESSION['cart']);
+        unset($_SESSION['cart.qty']);
+        unset($_SESSION['cart.sum']);
+        $this->loadView('cart_modal');
+        return true;
+    }
+
+}
---
+++ vendor/wfm/helpers/functions.php
@@ -0,0 +1,86 @@
+<?php
+
+function debug($data, $die = false)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+    if ($die) {
+        die;
+    }
+}
+
+function h($str)
+{
+    return htmlspecialchars($str);
+}
+
+function redirect($http = false)
+{
+    if ($http) {
+        $redirect = $http;
+    } else {
+        $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
+    }
+    header("Location: $redirect");
+    die;
+}
+
+function base_url()
+{
+    return PATH . '/' . (\wfm\App::$app->getProperty('lang') ? \wfm\App::$app->getProperty('lang') . '/' : '');
+}
+
+/**
+ * @param string $key Key of GET array
+ * @param string $type Values 'i', 'f', 's'
+ * @return float|int|string
+ */
+function get($key, $type = 'i')
+{
+    $param = $key;
+    $$param = $_GET[$param] ?? '';
+    if ($type == 'i') {
+        return (int)$$param;
+    } elseif ($type == 'f') {
+        return (float)$$param;
+    } else {
+        return trim($$param);
+    }
+}
+
+/**
+ * @param string $key Key of POST array
+ * @param string $type Values 'i', 'f', 's'
+ * @return float|int|string
+ */
+function post($key, $type = 's')
+{
+    $param = $key;
+    $$param = $_POST[$param] ?? '';
+    if ($type == 'i') {
+        return (int)$$param;
+    } elseif ($type == 'f') {
+        return (float)$$param;
+    } else {
+        return trim($$param);
+    }
+}
+
+function __($key)
+{
+    echo \wfm\Language::get($key);
+}
+
+function ___($key)
+{
+    return \wfm\Language::get($key);
+}
+
+function get_cart_icon($id)
+{
+    if (!empty($_SESSION['cart']) && array_key_exists($id, $_SESSION['cart'])) {
+        $icon = '<i id="del-' . (int)$id . '" class="fas fa-luggage-cart"></i>';
+    } else {
+        $icon = '<i id="del-' . (int)$id . '" class="fas fa-shopping-cart"></i>';
+    }
+    return $icon;
+}
