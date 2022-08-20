88da11897a569ff70dc9a7be2432ba2afd7f1a23
snuber-w <106393188+snuber-w@users.noreply.github.com>
2022-08-20 09:01:49 +0300

    alpha_v0.34_35

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -18,15 +18,47 @@ class UserController extends AppController
         if (!empty($_POST)) {
             $data = $_POST;
             $this->model->load($data);
-            if (!$this->model->validate($data)) {
+            if (!$this->model->validate($data) || !$this->model->checkUnique()) {
                 $this->model->getErrors();
+                $_SESSION['form_data'] = $data;
             } else {
-                $_SESSION['success'] = ___('user_signup_success_register');
+                $this->model->attributes['password'] = password_hash($this->model->attributes['password'], PASSWORD_DEFAULT);
+                if ($this->model->save('user')) {
+                    $_SESSION['success'] = ___('user_signup_success_register');
+                } else {
+                    $_SESSION['errors'] = ___('user_signup_error_register');
+                }
+
             }
             redirect();
         }
 
         $this->setMeta(___('tpl_signup'));
+    }
+
+    public function loginAction() {
+        if (User::checkAuth()) {
+            redirect(base_url());
+        }
+
+        if (!empty($_POST)) {
+            if ($this->model->login()) {
+                $_SESSION['success'] = ___('user_login_success_login');
+                redirect(base_url());
+            } else {
+                $_SESSION['errors'] = ___('user_login_error_login');
+                redirect();
+            }
+        }
+
+        $this->setMeta(___('tpl_login'));
+    }
+
+    public function logoutAction() {
+        if (User::checkAuth()) {
+            unset($_SESSION['user']);
+        }
+        redirect(base_url() . 'user/login');
     }
 
 }
