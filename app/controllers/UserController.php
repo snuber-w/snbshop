1640a251f272b3d76de6a3a9d752991b5351c832
snuber-w <106393188+snuber-w@users.noreply.github.com>
2022-08-22 10:21:06 +0300

    alpha_v0.40

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -61,4 +61,11 @@ class UserController extends AppController
         redirect(base_url() . 'user/login');
     }
 
+    public function cabinetAction() {
+        if (!User::checkAuth()) {
+            redirect(base_url() . 'user/login');
+        }
+        $this->setMeta(___('tpl_cabinet'));
+    }
+
 }
