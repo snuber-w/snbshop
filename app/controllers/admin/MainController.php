df0b5fe0fe2c5189dfa80b22f5d4a7ac50af60e1
snuber-w <106393188+snuber-w@users.noreply.github.com>
2022-09-02 09:41:20 +0300

    alpha_v0.99.02

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -6,7 +6,9 @@ class MainController extends AppController
 {
 
     public function indexAction() {
-
+        $title = 'HomePage';
+        $this->setMeta('Admin.:.HomePage');
+        $this->set(compact('title'));
     }
 
 }
