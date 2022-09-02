542096bed4204dae3f0e94186834bbbde77318f2
snuber-w <106393188+snuber-w@users.noreply.github.com>
2022-09-02 19:41:31 +0300

    alpha_v0.99.04

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -2,13 +2,20 @@
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
+
 class MainController extends AppController
 {
 
     public function indexAction() {
+        $orders = R::count('orders');
+        $new_orders = R::count('orders', 'status = 0');
+        $users = R::count('user');
+        $products = R::count('product');
+
         $title = 'HomePage';
         $this->setMeta('Admin.:.HomePage');
-        $this->set(compact('title'));
+        $this->set(compact('title', 'orders', 'new_orders', 'users', 'products'));
     }
 
 }
