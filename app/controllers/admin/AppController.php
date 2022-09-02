95c9f14049635efd38bf7d1a9f00c613e5b9c301
snuber-w <106393188+snuber-w@users.noreply.github.com>
2022-09-02 19:20:33 +0300

    alpha_v.0.99.03

--- app/controllers/admin/AppController.php
+++ app/controllers/admin/AppController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers\admin;
 
+use app\models\admin\User;
 use app\models\AppModel;
 use app\widgets\language\Language;
 use wfm\App;
@@ -15,6 +16,11 @@ class AppController extends Controller
     public function __construct($route)
     {
         parent::__construct($route);
+
+        if (!User::isAdmin() && $route['action'] != 'login-admin') {
+            redirect(ADMIN . '/user/login-admin');
+        }
+
         new AppModel();
         App::$app->setProperty('languages', Language::getLanguages());
         App::$app->setProperty('language', Language::getLanguage(App::$app->getProperty('languages')));
---
+++ app/controllers/admin/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\controllers\admin;
+
+use app\models\admin\User;
+
+/** @property User $model */
+class UserController extends AppController
+{
+    public function loginAdminAction() {
+        if ($this->model::isAdmin()) {
+            redirect(ADMIN);
+        }
+
+        $this->layout = 'login';
+        if (!empty($_POST)) {
+            if ($this->model->login(true)) {
+                $_SESSION['success'] = 'Вы успешно авторизованы';
+            } else {
+                $_SESSION['errors'] = 'Ошибка авторизации';
+            }
+            if ($this->model::isAdmin()) {
+                redirect(ADMIN);
+            } else {
+                redirect();
+            }
+        }
+    }
+
+    public function logoutAction() {
+        if ($this->model::isAdmin()) {
+            unset($_SESSION['user']);
+        }
+        redirect(ADMIN . '/user/login-admin');
+    }
+
+}
